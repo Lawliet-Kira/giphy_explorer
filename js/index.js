e264d27f55ef8b608d9419431cc46bb887f596a4
Lawliet-Kira <diego.reinoso@sansano.usm.cl>
2022-09-21 01:11:55 -0300

    second commit

---
+++ js/index.js
@@ -0,0 +1,17 @@
+let inputElem = document.getElementById('giphy__search_form');
+
+inputElem.onsubmit = (e) => {
+
+    e.preventDefault();
+
+    let valueForSearch = e.target.elements.giphy__search.value;
+
+
+    // fetch(" ")
+    //     .then(() => {
+
+    //     })
+    //     .catch(() => {
+
+    //     })
+};
